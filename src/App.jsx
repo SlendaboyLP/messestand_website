bec1d034f8fadcea91277dea04d5a0bf1030a8af
Alexander Zeitlhofer <alexander.zeitlhofer@sz-ybbs.ac.at>
2024-05-27 15:13:58 +0200

    fixxed tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
-import "./App.css";
-
 function App() {
   return (
-    <>
-      <h1>MesseStand</h1>
-    </>
+    <div className="flex items-center justify-center h-screen">
+      <h1 className="text-4xl font-bold text-blue-500">Hello, Tailwind CSS!</h1>
+    </div>
   );
 }
 
