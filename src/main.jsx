5637dd3efc73794e9d4414c0cef2c699cb870265
Alexander Zeitlhofer <alexander.zeitlhofer@sz-ybbs.ac.at>
2024-05-29 19:15:12 +0200

    changed title to "messestand"

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import NewsContent from "./NewsContent.jsx";
@@ -9,7 +10,6 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    
     <Router>
       <Routes>
         <Route path="/" element={<App />} />
